6c02f3c984cccfd6a2020f8a6d09cddd04cb2580
luizhtl <ltamalu@hotmail.com>
2024-01-31 18:27:55 -0300

    outra aula

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let ntentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
 let campo = document.querySelector(tag);
@@ -10,10 +11,30 @@ exibirTextoNaTela('p', 'escolha um numero entre 1 e 10');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    console.log(numeroSecreto);
-    console.log(chute == numeroSecreto);
+
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou');
+        let palavraTentativa = ntentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `voce descobriu o numero com ${ntentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p','o numero secreto é menor');
+        }else{
+            exibirTextoNaTela('p','o numero secreto é maior');
+        }
+        ntentativas++;
+        limparCampo();
+    }
+   
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
 }
